eea62e426451033c586cb150ceeaeb98b16a48f5
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2022-12-05 21:31:17 -0300

    Agregando codigo Python

---
+++ hello.py
@@ -0,0 +1,11 @@
+num = int(input("Ingresa un 1 si estas registrado en la pase de datos, sino ingresa un 0: "))
+name = input("Name: ")
+lasName = input("Lasname: ")
+
+if num > 0:
+    print(name + " " + lasName)
+    
+else: 
+    print("Lo siento, no estas en mi base de datos")
+
+hola = input("parar consola.")
